0764504ac1c35325c25fe73de9f8d692314e7cf3
Hlyrnir <tryggvi.magnusson@proton.me>
2025-01-10 21:01:08 +0100

    Passport.Application: Health check added

---
+++ src/Passport.Application/Query/HealthCheck/PassportHealthCheckQuery.cs
@@ -0,0 +1,10 @@
+﻿using Mediator;
+using Passport.Abstraction.Result;
+
+namespace Passport.Application.Query.HealthCheck
+{
+    public sealed class PassportHealthCheckQuery : IQuery<IMessageResult<bool>>
+    {
+
+    }
+}
---
+++ src/Passport.Application/Query/HealthCheck/PassportHealthCheckQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using Passport.Abstraction.Result;
+using Passport.Application.Default;
+using Passport.Application.Interface;
+using Passport.Application.Result;
+
+namespace Passport.Application.Query.HealthCheck
+{
+    public sealed class PassportHealthCheckQueryHandler : IQueryHandler<PassportHealthCheckQuery, IMessageResult<bool>>
+    {
+        private readonly IUnitOfWork uowUnitOfWork;
+
+        public PassportHealthCheckQueryHandler([FromKeyedServices(DefaultKeyedServiceName.UnitOfWork)] IUnitOfWork uowUnitOfWork)
+        {
+            this.uowUnitOfWork = uowUnitOfWork;
+        }
+
+        public async ValueTask<IMessageResult<bool>> Handle(PassportHealthCheckQuery qryQuery, CancellationToken tknCancellation)
+        {
+            bool bIsHealthy = false;
+
+            try
+            {
+                await uowUnitOfWork.TransactionAsync(() =>
+                {
+                    bIsHealthy = uowUnitOfWork.TryRollback();
+
+                    return Task.CompletedTask;
+                });
+            }
+            catch (Exception exException)
+            {
+                return new MessageResult<bool>(new MessageError() { Code = "EXCEPTION", Description = exException.Message });
+            }
+
+            return new MessageResult<bool>(bIsHealthy);
+        }
+    }
+}
